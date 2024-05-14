b006bf8c5b5939a5089fb534c76fb349776c34ff
Sazeebul Bashar <sazeebul@gmail.com>
2024-05-15 02:58:26 +0600

    modified build

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-python3.11 -m pip install -r requirements.txt 
-python3.11 manage.py collectstatic --no-input  --clear 
+python -m pip install -r requirements.txt 
+python manage.py collectstatic --no-input  --clear 
