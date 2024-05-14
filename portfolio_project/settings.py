8e268cd0db0e33b66f028c830d96aef1c2a135fc
Sazeebul Bashar <sazeebul@gmail.com>
2024-05-15 02:33:59 +0600

    bug fixes

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR /'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
